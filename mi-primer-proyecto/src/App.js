bb457e2282658c467a1876893ab3da8e809346b9
martinadero <mdaireaux@udesa.edu.ar>
2023-04-03 12:39:36 -0300

    modificaciones en el app

--- mi-primer-proyecto/src/App.js
+++ mi-primer-proyecto/src/App.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import header from './Components/header/header';
+import Footer from './Components/footer/footer';
+import home from './screens/home/home';
+
+
 function App() {
   return (
     <div className="App">
