6a118cf1b4d4333e3d91daa75e4686677d2a7773
priscillaarslan <parslan@udesa.edu.ar>
2023-04-03 15:43:27 -0300

    agregue

--- mi-primer-proyecto/src/screens/Favoritos/favoritos.js
+++ mi-primer-proyecto/src/screens/Favoritos/favoritos.js
@@ -12,52 +12,7 @@ class Favoritos extends Component {
             loader: true
         };
     };
-}
-
-componentDidMount() {
-    let recuperoStorage = localStorage.getItem('favoritos');
-    let favoritosToArray;
-
-    if (recuperoStorage !== null) {
-        favoritosToArray = JSON.parse(recuperoStorage);
-        let peliculas = []
-        
-        for(let i = 0; i < favoritosToArray.length; i++){
-            if(favoritosToArray[i] !== null){
-                fetch(`https://api.themoviedb.org/3/movie/${favoritosToArray[i]}?api_key=1845c94396255a256363182ed898e8fc&language=en-US`)
-                .then(res => res.json())
-                .then(data => {
-                    canciones.push(data)
-                    this.setState({
-                        canciones: canciones,
-                        loader: false
-                    });
-                })
-                .catch(err => console.log(err))
-            }
-        }
-        
-    };
-    
-    if (JSON.parse(recuperoStorage).length === 0){
-        this.setState({
-            loader: false
-        })
-    }
-    
-}
-
-borrar(id){
-    let recuperoStorage = localStorage.getItem('favoritos');
-    let favoritosToArray = JSON.parse(recuperoStorage);
-
-    let sacarFav = favoritosToArray.indexOf(id);
-    favoritosToArray.splice(sacarFav, 1);
-
-    let favoritosToString = JSON.stringify(favoritosToArray);
-    localStorage.setItem('favoritos', favoritosToString);
-}
-
+};
 //AGREGAR RENDER//
 
 export default Favoritos;
