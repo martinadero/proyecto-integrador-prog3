bee5b7826b02f41bb5cbd50539a3b6aaf909c769
priscillaarslan <parslan@udesa.edu.ar>
2023-04-03 10:31:56 -0300

    agregue

--- mi-primer-proyecto/src/Components/footer/footer.js
+++ mi-primer-proyecto/src/Components/footer/footer.js
@@ -3,9 +3,10 @@ import React from "react";
 function Footer(){
     return(
         <footer>
-        <p>Martina Mendez, Martina Daireaux, Priscilla Arslan</p>
+        <p>Martina Mendez, Martina Daireaux, hola</p>
     </footer>
     )
 }
 
 export default Footer;
+
