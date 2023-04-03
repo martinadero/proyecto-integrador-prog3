fd1e5f3560cc3a90a3e997b51c873baedec88ca7
priscillaarslan <parslan@udesa.edu.ar>
2023-04-03 16:00:50 -0300

    agreguenotfound

--- mi-primer-proyecto/src/screens/home/home.js
+++ mi-primer-proyecto/src/screens/home/home.js
@@ -10,9 +10,28 @@ class Home extends Component {
             resultadoDeBusqueda: [],
             valor: props.value, 
         };
-    };
+    }
 
 
+    componentDidMount() {
+        //Buscamos datos
+        fetch('https://api.allorigins.win/raw?url=https://api.deezer.com/track/${id}/')
+            .then(res => res.json())
+            .then(data => this.setState({
+                canciones: data.results,
+                loader: false
+            }))
+            .catch(err => console.log(err))
+
+        fetch('https://api.allorigins.win/raw?url=https://api.deezer.com/track/${id}/') //CAMBIAR ESTE LINK//
+            .then(response => response.json())
+            .then(data => this.setState({
+                canciones: data.results,
+                loader: false
+
+            }))
+            .catch(error => console.log(error));
+    }
 
 }
 
