cca70298b677828bfd67eee7298e32b794b06a14
priscillaarslan <parslan@udesa.edu.ar>
2023-04-03 12:03:11 -0300

    agregue

---
+++ mi-primer-proyecto/src/screens/home/home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            props: props ,
+            valor:0, 
+            canciones: [],
+            resultadoDeBusqueda: [],
+        };
+    };
+
+
+
+}
+
+export default Home;
