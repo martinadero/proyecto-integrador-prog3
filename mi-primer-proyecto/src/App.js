5ffc0ce882b56a31a2aa2cd3c5b3c3f880aaa1c4
martinadero <mdaireaux@udesa.edu.ar>
2023-04-06 22:32:08 -0300

    viendo header

--- mi-primer-proyecto/src/App.js
+++ mi-primer-proyecto/src/App.js
@@ -3,6 +3,7 @@ import Home from './screens/home/home';
 import Detalle from './screens/detalle/detalle'
 import Footer from './Components/footer/footer';
 import { Route, Switch,} from 'react-router-dom';
+import Header from './Components/header/header'
 function App() {
   return (
     <div className="App">
@@ -10,7 +11,7 @@ function App() {
        <Route path='/' component={Home}/>
        <Route path='/detallePelicula/id/:id'component={Detalle} />
     </Switch>
-      
+      <Header/>
    <Footer/>
    
        </div>
