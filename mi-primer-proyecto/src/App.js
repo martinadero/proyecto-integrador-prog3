a3f35a5d24143841363bcbd9c9d2a4fba84b5c99
martinadero <mdaireaux@udesa.edu.ar>
2023-04-06 22:25:32 -0300

    modificaciones

--- mi-primer-proyecto/src/App.js
+++ mi-primer-proyecto/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Tarjeta from './Components/tarjeta/tarjeta';
 import Home from './screens/home/home';
-import Detalle from './screens/Detalle/detalle'
+import Detalle from './screens/detalle/detalle'
 import Footer from './Components/footer/footer';
-import { Route, Switch,Link} from 'react-router-dom';
+import { Route, Switch,} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
--- mi-primer-proyecto/src/screens/home/home.js
+++ mi-primer-proyecto/src/screens/home/home.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Tarjeta from '../../Components/tarjeta/tarjeta';
 import './home.css'; 
 
-
 class Home extends Component {
     constructor(props){
         super(props);
@@ -12,17 +11,18 @@ class Home extends Component {
             loader:true
          };
     }
-componentDidMount() {
+    componentDidMount() {
         //Buscamos datos
-      fetch('https://api.themoviedb.org/3/movie/popular?api_key=a0959ac201dc94da76d17af9fee2bfd2&language=en-US&page=1') //CAMBIAR ESTE LINK//
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.results);
-                 this.setState({
-                peliculas: data.results,
-                loader: false
-             })})
-            .catch(error => console.log(error));
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=a0959ac201dc94da76d17af9fee2bfd2&language=en-US&page=1') //CAMBIAR ESTE LINK//
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.results);
+                    this.setState({
+                        peliculas: data.results,
+                        loader: false
+                    })
+                })
+                .catch(error => console.log(error));
     }
     filtrar(){
         console.log('esta funcionando');
@@ -31,18 +31,14 @@ componentDidMount() {
         console.log(this.state.peliculas);
         return(
             <>
-          <h1 className='home' >Pagina del home</h1>
-           {
-            this.state.peliculas.map((pelicula)=>
-            <Tarjeta 
-            data = {pelicula}
-            key={ pelicula.id} 
-           />
-
-            )
-           }
-         </>
-         )
+                <h1 className='home' >Pagina del home</h1>
+                <div className='home-conteiner'>
+                    {
+                        this.state.peliculas.map( pelicula => <Tarjeta data={pelicula} key={pelicula.id} />)
+                    }
+                </div>
+            </>
+        )
     }
 
 }
