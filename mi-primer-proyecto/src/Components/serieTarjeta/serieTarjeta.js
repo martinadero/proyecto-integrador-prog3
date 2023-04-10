ecc434e91eeb62ef78957893111cf7ad4c141dae
priscillaarslan <parslan@udesa.edu.ar>
2023-04-10 12:05:47 -0300

    agregue

--- mi-primer-proyecto/src/Components/serieTarjeta/serieTarjeta.js
+++ mi-primer-proyecto/src/Components/serieTarjeta/serieTarjeta.js
@@ -7,8 +7,15 @@ class serieTarjeta extends Component {
         this.state = {};
     }
     render(){
-        
-    
+        console.log(this.props);
+        return (
+                <div className='tarjetas'>
+                    <h3 className='textotarjeta'>{this.props.data.overview}</h3>
+                    <h3 className='textotarjeta'>{this.props.data.name}</h3>
+                    <h3 className='textotarjeta'>{this.props.data.first_air_date}</h3>
+                    <img className='imagenes' src={`poster_path: "/64xJnzk6z6Oj6dKha2h4JxgDy6x.jpg"}`} alt={this.props.data.poster_path} />
+                </div>
+        )
     }
 }
 
