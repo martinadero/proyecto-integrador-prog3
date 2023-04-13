86bad6f17d5f788310037e0e413cd145b9d3abab
martinadero <mdaireaux@udesa.edu.ar>
2023-04-13 11:11:26 -0300

    agrego

--- mi-primer-proyecto/src/Components/header/header.js
+++ mi-primer-proyecto/src/Components/header/header.js
@@ -11,10 +11,7 @@ function Header(props){
                 <Link className='homelogo' to='/'><img className='logo' src='./img/logo.png' alt='' /></Link>
             </div>
             <div className='search'>
-                <form className='search-form'>
-                    <input type='text' placeholder='Buscar' onChange={props.filtrar} className='search-form_input'></input>
-                    <button onClick={props.filtrar} className='search-form_button'>&#128269;</button>
-                </form>
+             
             </div>
             <div className="headerlist">
                 <Link className='textoheader' to='/' style={{ textDecoration: 'none' }}><p>HOME</p></Link>
